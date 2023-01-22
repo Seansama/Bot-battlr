651472ecbfdc4732d04a34b92ab0b9a77ea74480
Shaun <seansama@pop-os.localdomain>
2023-01-22 20:28:45 +0300

    Created bot type class

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -1,2 +1,10 @@
 import React from "react";
 
+const botTypeClasses = {
+    Assault: "icon military",
+    Defender: "icon shield",
+    Support: "icon plus circle",
+    Medic: "icon ambulance",
+    Witch: "icon magic",
+    Captain: "icon star",
+};
