e7c776a1b9cd4adba0c1f2cbdbd7d4218e963774
Shaun <seansama@pop-os.localdomain>
2023-01-22 21:54:50 +0300

    Exported YourBotArmy component

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,6 +1,18 @@
 import React from "react";
 import BotCard from "./BotCard";
 
-function YourBotArmy() {
-    
+function YourBotArmy({botArmy, addRemoveBot}) {
+    return (
+        <div className="ui segment inverted olive bot-army">
+            <div className="ui five column grid">
+                <div className="row bot-army-row">
+                    {botArmy.map(bot => (
+                        <BotCard key={bot.id} bot={bot} addRemoveBot={addRemoveBot} army={true} />
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
 }
+
+export default YourBotArmy
