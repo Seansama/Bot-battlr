2ab25199bd473b7df1dc9193567a67adc7d39459
Shaun <seansama@pop-os.localdomain>
2023-01-22 21:12:26 +0300

    Created Botcard UI

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -9,6 +9,54 @@ const botTypeClasses = {
     Captain: "icon star",
 };
 
-function Botcard() {
-    
+function Botcard({bot}) {
+    return (
+        <div className="ui column">
+            <div
+                className="ui card"
+                key={bot.id}
+                onClick={() => console.log(bot.name)}
+            >
+                <div className="image">
+                    <img alt="oh no!" src={bot.avatar_url}/>
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {bot.name}
+                        <i className={botTypeClasses[bot.bot_class]}/>
+                    </div>
+                    <div className="meta text-wrap">
+                        <small>{bot.catchphrase}</small>
+                    </div>
+                </div>
+                <div className="extra content">
+          <span>
+            <i className="icon heartbeat"/>
+              {bot.health}
+          </span>
+
+                    <span>
+            <i className="icon lightning"/>
+                        {bot.damage}
+          </span>
+                    <span>
+            <i className="icon shield"/>
+                        {bot.armor}
+          </span>
+                    <span>
+            <div className="ui center aligned segment basic">
+              <button
+                  className="ui mini red button"
+                  onClick={() =>
+                      console.log("add code to connect event listener")
+                  }
+              >
+                x
+              </button>
+            </div>
+          </span>
+                </div>
+            </div>
+        </div>
+    );
 }
