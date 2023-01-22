634778503c32faf9c50088fdb50f590975e5cad7
Shaun <seansama@pop-os.localdomain>
2023-01-22 21:35:59 +0300

    Add or Remove Bot

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import BotCard from "./BotCard";
 
-function BotCollection() {
-    
+function BotCollection({botCollection, addOrRemoveBot}) {
+    return (
+        <div className="ui four column grid">
+            <div className="row">
+                {botCollection.map(bot => (
+                    <BotCard key={bot.id} bot={bot} addOrRemoveBot={addOrRemoveBot} />
+                ))}
+            </div>
+        </div>
+    );
 }
+
